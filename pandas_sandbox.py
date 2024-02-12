0de12c39f7c4e8dd6ea85a0e2bc737ead784cd23
maumayagoitia <157406571+maumayagoitia@users.noreply.github.com>
2024-02-12 11:24:38 -0700

    Create pandas_sandbox.py

---
+++ pandas_sandbox.py
@@ -0,0 +1,11 @@
+import pandas
+
+list_teams = ['49ers','KC','Cowboys','Steelers']
+
+print(type(list_teams))
+
+series_teams = pandas.Series(list_teams)
+
+print(series_teams)
+print(type(series_teams))
+              
