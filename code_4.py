b8f5a54e23f756b2301086d478d1c6006dff7b54
maumayagoitia <157406571+maumayagoitia@users.noreply.github.com>
2024-02-16 16:12:02 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -2,11 +2,13 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
-
+print("hello")
 def get_big_mac_price_by_year(year,country_code):
     query = f"year == {year} and iso_a3 == '{country_code}'"
     result = df.query(query)[['name', 'iso_a3', 'dollar_price']]
-    return round(result['dollar_price'].mean(), 2) if not result.empty else None
+    mean_price = result['dollar_price'].mean()
+    return round(mean_price, 2)
+    
 
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
