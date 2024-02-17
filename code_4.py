20ad39da8b3ac57b85be518615662327aaab6e2f
maumayagoitia <157406571+maumayagoitia@users.noreply.github.com>
2024-02-16 21:07:43 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -7,7 +7,7 @@ df = pd.read_csv(big_mac_file)
 def get_big_mac_price_by_year(year, country_code):
     query = f"date.str.startswith('{year}') and iso_a3 == '{country_code}'"
     result = df.query(query)
-    mean_price = result['{dollar_price}'].mean()
+    mean_price = result['dollar_price'].mean()
     return round(mean_price, 2)
 
 def get_big_mac_price_by_country(country_code):
