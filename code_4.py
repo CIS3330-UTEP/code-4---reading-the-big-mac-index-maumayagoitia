2d4710fd2f17316b8991ec91f2738f637ae8bae1
maumayagoitia <157406571+maumayagoitia@users.noreply.github.com>
2024-02-21 10:55:15 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -5,12 +5,14 @@ big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year, country_code):
+    country_code = country_code.upper()
     query = f"date.str.startswith('{year}') and iso_a3 == '{country_code}'"
     result = df.query(query)
     mean_price = result['dollar_price'].mean()
     return round(mean_price, 2)
 
 def get_big_mac_price_by_country(country_code):
+    country_code = country_code.upper()
     query = f"iso_a3 == '{country_code}'"
     result = df.query(query)
     mean_price = result['dollar_price'].mean()
